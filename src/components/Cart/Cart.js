cfa73fef9a1e4b6bd6ff21cb1c113ef5b2d2ef96
mohon63 <mohonhossen63@gmail.com>
2021-09-26 16:33:01 +0600

    provlem solution

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,13 +6,15 @@ const Cart = (props) => {
     const total = cart.reduce(totalReduce, 0);
     return (
         <div>
-            <h4><i className="fa fa-user"></i> Programmer added: {cart.length}</h4>
-            <h4>Total Cost: ${total.toFixed(2)}</h4>
-            <ul>
-                {
-                    cart.map(programmer => <li>{programmer.name}</li>)
-                }
-            </ul>
+            <div className="border border-secondary h-100 bg-white p-3 cart rounded">
+                <h4><i className="fa fa-user"></i> Programmer added: {cart.length}</h4>
+                <h4>Total Cost: ${total.toFixed(2)}</h4>
+                <ul>
+                    {
+                        cart.map(programmer => <li className="list-unstyled">{programmer.name}</li>)
+                    }
+                </ul>
+            </div>
         </div>
     );
 };
